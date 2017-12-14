40c4aa38023c08ccfec4374ab7b9c01f8b1b916d
Rawand Hawiz <r.hawiz@geophy.com>
2017-12-14 13:10:47 +0100

    msg_obj added to parse objects

--- logmodule/udphandler.py
+++ logmodule/udphandler.py
@@ -69,6 +69,7 @@ class UdpHandler(logging.handlers.DatagramHandler):  # Inherit from logging.Hand
         msg = d.get("msg", record.getMessage())
 
         d['msg'] = msg
+        d['msg_obj'] = msg
         d['args'] = None
         d['exc_info'] = None
         # Issue #25685: delete 'message' if present: redundant with 'msg'
